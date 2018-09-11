24f1c9bf196f12a500a7239cf84de5cc5d1231aa
kinyua <kaigikinyua@gmail.com>
2018-09-11 22:30:52 +0300

    the function also evaluates float but there is some stack smashing

--- BConverter.cpp
+++ BConverter.cpp
@@ -1,37 +1,70 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-int main()
-{
-  int binary[16];
-  cout<<"Enter an integer"<<endl;
-  int n;
-  cin>>n;
-  int p=0,i=0;
+void Whole(float num){
+  int binary[16];  float n=num; int p=0,i=0;int decimal[16];
   while(pow(2,p)<n){
     p++;
-    cout <<p<<endl;
+    //cout<<p<<endl;
   }
-  p=p-1;
   int number=p;
   while(n>0){
+    if (0<n and n<1){
+      int p=-1;int i=0;
+      while(pow(2,p)<n){
+        p++;
+        //cout<<p<<endl;
+      }  p=p-1;
+      while(n>0){
+        if(n>pow(2,p)){
+          n=n-pow(2,p);
+          p=p-1;
+          decimal[i]=1;
+        }else{
+          p=p-1;
+          binary[i]=0;
+        }
+        i++;
+        if(n<=0){
+          break;
+        }
+      }
+  }else{
     if(n>=pow(2,p)){
       n=n-pow(2,p);
       p=p-1;
       binary[i]=1;
-      cout<<binary[i]<<endl;
+      //cout<<binary[i]<<endl;
     }else{
       p=p-1;
       binary[i]=0;
-      cout<<binary[i]<<endl;
+      //cout<<binary[i]<<endl;
     }
-    i++;
-    //cout <<i<<endl;
+      i++;
   }
-  //conversion
+}
   for (int j=0;j<=number;j++){
     cout<<binary[j];
   }
   cout<<"\n"<<endl;
+  for (int j=0;j<=number;j++){
+    cout<<decimal[j];
+  }
+  cout<<"\n"<<endl;
+}
+int main()
+{
+  int ans;
+  cout<<"Enter a choice \n1.Integer\n2.Decimal"<<endl;
+  cin>>ans;cout<<"\n"<<endl;
+  if (ans==1){
+    cout<<"Enter the number"<<endl;
+    float n;cin>>n;
+    Whole(n);
+  }else if(ans==2){
+    //call the decimal function
+  }else {
+    cout<<"Unknown Choice"<<endl;
+  }
   return 0;
 }
