f60cc94707eb3c802a2692c37b5d93d1bbd0cf1f
kinyua <kaigikinyua@gmail.com>
2018-09-23 16:59:06 +0300

    can only convert even binary correctly

--- BConverter.cpp
+++ BConverter.cpp
@@ -2,8 +2,21 @@
 #include<math.h>
 using namespace std;
 //takes binary and dispalys the equivalent decimal
-void bin2deci(int bin[16]){
-
+void bin2deci(int bin,int j){
+  int ans=0;int i=j;
+  //cout<<j<<endl;
+ while(i>0){
+   if((bin-pow(10,i))>0){
+     cout<<bin<<" binary "<<endl;
+     ans+=pow(2,i);
+     bin=bin-pow(10,i);
+     cout<<"ans "<<i<<" bin "<<bin<<endl;
+   }else if((bin-pow(10,i))==1){
+     ans+=1;
+   }
+   i--;
+ }
+ cout<<ans<<endl;
 }
 void Whole(float num){
   int binary[16];  float n=num; int p=0,i=0;int decimal[16];
@@ -57,8 +70,22 @@ void Whole(float num){
 }
 int main()
 {
-  cout<<"Enter the number"<<endl;
-  float n;cin>>n;
-  Whole(n);
+  cout<<"Option 1 change from decimal to binary\nOption 2 change from binary to decimal\n\n"<<endl;
+  int choice;cin>>choice;
+  if (choice==1){
+    float n;cin>>n;
+    Whole(n);
+  }else if (choice==2){
+    cout<<"Enter the binary digits\n"<<endl;
+    int bin;cin>>bin;int j=1;int i;
+    for(i=0;i<=16;i++){
+      if(j<=bin){
+        j*=10;
+      }else{break;}
+    }
+    bin2deci(bin,i);
+  }else{
+    cout<<"Uknown choice"<<endl;
+  }
   return 0;
 }
